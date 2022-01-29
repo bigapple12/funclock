18cde1dbbfb3753e293d34cf61de28fec5317a22
bigapple12 <8504147+bigapple12@users.noreply.github.com>
2022-01-29 17:37:00 +0900

    create clock

---
+++ android/app/src/main/kotlin/com/example/funclock/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.funclock
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homepage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:funclock/clock_view.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        color: const Color(0xFF2D2F41),
+        child: const ClockView(),
+      ),
+    );
+  }
+}
